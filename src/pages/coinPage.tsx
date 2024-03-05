f9b532989b3601851a09e5d8c488e0314ae0bad1
aayush <sharma31aayush@gmail.com>
2024-03-05 21:10:55 +0530

    feat: add toggleSection, CardPromo and tradeViewGraphs

--- src/pages/coinPage.tsx
+++ src/pages/coinPage.tsx
@@ -1,9 +1,21 @@
+import CardPromo from '../components/CardPromo';
+import ToggleSection from '../components/ToggleSecion';
+import Crypto from '../components/crypto';
 import PageHeader from '../components/pageHeader';
 
 const CoinPage = () => {
   return (
     <div className='h-full w-screen bg-slate-200/40'>
       <PageHeader />
+      <div className='w-screen flex'>
+        <div className='w-8/12'>
+          <Crypto />
+          <ToggleSection />
+        </div>
+        <div className='4/12'>
+          <CardPromo />
+        </div>
+      </div>
     </div>
   );
 };
