fb94319182f5f19943c3d51ef4d47e15ed2e6187
aayush <sharma31aayush@gmail.com>
2024-03-06 13:59:32 +0530

    add: Tokenomics component and add components to coinPage

--- src/pages/CoinPage.tsx
+++ src/pages/CoinPage.tsx
@@ -4,7 +4,9 @@ import Crypto from '../components/Crypto';
 import PageHolder from '../components/PageHolder';
 import PerformanceSection from '../components/PerformanceSection';
 import SentimentSection from '../components/SentimentSection';
+import TeamCard from '../components/TeamCard';
 import ToggleSection from '../components/ToggleSection';
+import Tokenomics from '../components/Tokenomics';
 import TrendingCoins from '../components/TrendingCoins';
 
 const CoinPage = () => {
@@ -18,6 +20,8 @@ const CoinPage = () => {
           <PerformanceSection />
           <SentimentSection />
           <AboutBitcoinCard />
+          <Tokenomics />
+          <TeamCard />
         </div>
         <div className='w-4/12 mr-14'>
           <CradsPromo />
